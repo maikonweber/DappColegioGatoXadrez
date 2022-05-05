3841b9e823a6a98f82cfa8dcc136a32757dca10c
Maikon <m.carvalho@grouplinknetwork.com>
2022-05-05 18:17:56 -0300

    add

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -8,55 +8,20 @@ import { nftaddress, nftmarketaddres } from '../config/configWallet';
 import axios from 'axios';
 import { Grid,Box, Flex, Text, Button, GridItem } from '@chakra-ui/react';
 import Image from 'next/image'
-
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from '@chakra-ui/react'
 
 function Dashboard() {
   const [nfts, setNfts] = useState([])
   const [sold, setSold] = useState([])
   const [loadingState, setLoadingState] = useState('not-loaded')
-  useEffect(() => {
-    loadNFTs()
 
-  }, [])	
-  async function loadNFTs() {
-    const web3Modal = new Web3Modal(
-    //     {
-    //   network: "mainnet",
-    //   cacheProvider: true,
-    // }
-    )
-    const connection = await web3Modal.connect({
-    })
-    const provider = new ethers.providers.Web3Provider({connection,
-      theme: "dark"
-    })
-    const signer = provider.getSigner()
-    const marketContract = new ethers.Contract(nftmarketaddres, Market.abi, signer)
-    const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
-    const data = await marketContract.fetchmyNFTs()
-    
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await tokenContract.tokenURI(i.tokenId)
-      const meta = await axios.get(tokenUri)
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-      let item = {
-        price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        sold: i.sold,
-        image: meta.config.url,
-      }
-      return item
-    }))
-    /* create a filtered array of items that have been sold */
-    const soldItems = items.filter(i => i.sold)
-    setSold(soldItems)
-    setNfts(items)
-    setLoadingState('loaded')
-  }
-
-  
   return (
   <>
    <Flex 
@@ -64,57 +29,29 @@ function Dashboard() {
      height={['100vh', '100vh', '100vh', '100vh', '100vh']}
      alignItems='center'
      justifyContent='center'
-     bg='red.100'
+     bg='white'
    >
-    <Text> NFT Created  </Text>
-            <Grid 
-               templateColumns={['1fr', '1fr', '1fr', '1fr', '1fr']}
-               templeteRow={['1fr', '1fr', '1fr', '1fr', '1fr']}
-               gap={['1rem', '1rem', '1rem', '1rem', '1rem']}
-            >
-                {nfts.map(nft => (
-                   
-                    <>
-                    <Box 
-                    key={nft.tokenId}
-                    >
-                        <Image src={nft.image || ''} alt={nft.name}
-                            width="200px"
-                            height="200px">
-                            </Image>
-                        <Box>
-                        <Text>Price - {nft.price} ETH </Text>
-                        </Box>
-                    </Box>
-                    </>
-                ))}
-                </Grid>            
-             
-         {Boolean(sold.length) && (
-
-            <>
-            <Text> NFT Sold  </Text>
-            <Grid
-                templateColumns={['1fr', '1fr', '1fr', '1fr', '1fr']}
-                templeteRow={['1fr', '1fr', '1fr', '1fr', '1fr']}
-                gap={['1rem', '1rem', '1rem', '1rem', '1rem']}
-            >
-                {
-              sold.map((nft, i) => (
-                <Box
-                key={nft.tokenId}>
-                    <Image src={nft.image} alt={nft.name}
-                        width="200px"
-                        height="200px"></Image>
-                    <Box>
-                        <Text>Price - {nft.price} ETH </Text>
-                    </Box>
-                </Box>
-                ))
-            }
-             </Grid>
-                </> 
-         )}
+      <Box
+        width={['100%', '100%', '100%', '100%', '100%']}
+        height={['100%', '100%', '100%', '100%', '100%']}
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='start'
+        bg='white'
+        p={4}
+      >
+        <Text fontSize='2xl' fontWeight='bold' color='red.500'>
+          Dashboard
+        </Text>
+        
+        </Box>
+        <Stat>
+  <StatLabel>Collected Fees</StatLabel>
+  <StatNumber>£0.00</StatNumber>
+  <StatHelpText>Feb 12 - Feb 28</StatHelpText>
+</Stat>
+  
         </Flex>
         </>
   )
