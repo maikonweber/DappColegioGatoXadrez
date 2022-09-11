3b8a6678951b833e74b36e19c4d961219c977d95
Maikon <m.carvalho@grouplinknetwork.com>
2022-09-11 19:22:17 -0300

    Add New Lines

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme  from "../config/config";
 import Headers from '../src/components/Header';
 import Image from 'next/image';
-import Linker from '../src/components/Link';
 import Footer from '../src/components/Footer';
 import { AuthProvider } from '../src/context/authContext';
 
